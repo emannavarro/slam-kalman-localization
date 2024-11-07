1ee2effa89cb67444b77d95db6482296f9aa1aee
Emanuel Navarro-Ortiz <emannavarro@gmail.com>
2024-11-06 16:16:48 -0800

    Delete build directory

--- build/webots_ros2_mavic/colcon_command_prefix_setup_py.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/emannavarro/slam-kalman-localization/install/webots_ros2_importer/share/webots_ros2_importer/package.sh"
-. "/home/emannavarro/slam-kalman-localization/install/webots_ros2_msgs/share/webots_ros2_msgs/package.sh"
-. "/home/emannavarro/slam-kalman-localization/install/webots_ros2_driver/share/webots_ros2_driver/package.sh"
--- build/webots_ros2_turtlebot/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/emannavarro/slam-kalman-localization/install/webots_ros2_turtlebot'
